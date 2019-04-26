d1b1bd1b1fcb100220daa76d2b36e44c1bf94c8b
Juan Camilo Gonzalez Saavedra <jcgonzalez1081@misena.edu.co>
2019-04-26 15:57:57 -0500

    chore: propuesto07

--- examples/src/examples/Propuesto07.java
+++ examples/src/examples/Propuesto07.java
@@ -1,31 +1,5 @@
 package examples;
 
-import java.util.Scanner;
-
 public class Propuesto07 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int minuto = 60;
-		int segundo = 3600;
-
-		int t, h, m, s;
-
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("Tiempo en horas: ");
-		t = teclado.nextInt();
-
-		h = t;
-		t = t % minuto;
-		m = t * minuto;
-		s = t * segundo;
-
-		System.out.println("");
-		System.out.println("Horas: " + h);
-		System.out.println("Minutos: " + m);
-		System.out.println("Segundos: " + s);
-
-	}
 
 }
